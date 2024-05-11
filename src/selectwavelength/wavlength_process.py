5ae1e522bb9910abf8b4a549c8282477d92e3f32
jorgeeldis <jorgeeldisg30@gmail.com>
2024-05-11 14:44:59 -0500

    Added wavelgnth elect to interpolate

--- src/selectwavelength/wavlength_process.py
+++ src/selectwavelength/wavlength_process.py
@@ -20,29 +20,28 @@ DATA_SIZE = 7
 BASELINE_FILE = os.getenv("BASELINE_FILE")
 SINGLE_FILE = os.getenv("SINGLE_FILE")
 WAVELENGTH_FILE = os.getenv("WAVELENGTH_FILE")
+INTERPOLATE_FILE = os.getenv("INTERPOLATE_FILE")
 PORT = os.getenv("PORT")
 BAUDRATE = os.getenv("BAUDRATE")
 baseline_path = os.path.join("data", BASELINE_FILE)
 single_path = os.path.join("data", SINGLE_FILE)
 wavelength_path = os.path.join("data", WAVELENGTH_FILE)
+interpolate_path = os.path.join("data", INTERPOLATE_FILE)
 
 def get_line_value(nm):
-    with open(wavelength_path, 'r') as file:
-        lines = [float(line.strip()) for line in file.readlines()]
+    with open(interpolate_path, 'r') as file:
+        lines = [line.strip().split(',') for line in file.readlines()]
+        # Convert strings to float
+        lines = [(float(wavelength), float(absorbance)) for wavelength, absorbance in lines]
         # Find the line with the value closest to nm
-        closest_line_wavelength = min(range(len(lines)), key=lambda index: abs(lines[index]-nm))
-        closest_value_wavelength = lines[closest_line_wavelength]
+        closest_line_wavelength = min(range(len(lines)), key=lambda index: abs(lines[index][0]-nm))
+        closest_value_wavelength, closest_absorbance = lines[closest_line_wavelength]
         message = f"Line {closest_line_wavelength + 1} has the closest value: {closest_value_wavelength}"
-        return closest_value_wavelength, message
+        return closest_value_wavelength, closest_absorbance, message
 
 def get_absorbance(nm):
-    _, message = get_line_value(nm)
-    line_number = int(message.split()[1]) - 1  # Subtract 1 because list indices start at 0
-
-    with open(single_path, 'r') as file:
-        lines = [line.strip() for line in file.readlines()]
-        absorbance_value_select = lines[line_number]
-        return absorbance_value_select
+    _, absorbance, _ = get_line_value(nm)
+    return absorbance
         
 
 class SelectWavelength(QWidget):
