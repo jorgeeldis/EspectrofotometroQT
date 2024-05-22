0c0ba09e2feab4cc91d130f5ff0477556017e45f
Jorge Eldis <73083341+jorgeeldis@users.noreply.github.com>
2024-05-22 08:51:43 -0500

    Update absorption_calc.py

--- src/libs/absorption_calc.py
+++ src/libs/absorption_calc.py
@@ -2,5 +2,8 @@ import math
 
 
 def absorption(baseline, single):
-    calc = math.log10((baseline / single))*1.41
+    a = math.log10((baseline / single))
+    to = single/baseline
+    t = 1.060251*to-0.133634
+    calc = math.log10(1/t)
     return calc
